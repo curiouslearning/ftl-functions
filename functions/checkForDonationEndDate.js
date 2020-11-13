dfac8569b28fe5936ec27805f523ae56d4b4df3d
Eric GT <bookjunkie93@gmail.com>
2020-11-13 15:52:26 -0500

    checkDonationEndDate: all tests passing

--- functions/checkForDonationEndDate.js
+++ functions/checkForDonationEndDate.js
@@ -9,16 +9,23 @@ exports.checkForDonationEndDate = functions.firestore
       if (change.after.data().percentFilled >= 100) {
         let data = change.after.data();
         if (!data.hasOwnProperty('endDate')) {
+          data['endDate'] = admin.firestore.Firestore.Timestamp.now();
           return admin.firestore().collection('donor_master')
               .doc(context.params.donorId).collection('donations')
               .doc(context.params.documentId).set({
-                endDate: admin.firestore.Timestamp.now(),
+                endDate: data.endDate,
               }, {merge: true}).then(()=>{
-                return 'resolved';
+                return {status: 200, data: data};
+              }).catch((err)=>{
+                console.log(err);
+                return {
+                  status: 400,
+                  data: `encountered an error and could not continue: ${err}`,
+                };
               });
         }
-        return new Promise((resolve)=>{
-          resolve('resolved');
-        });
       }
+      return new Promise((res, rej)=>{
+        res({status: 200, data: 'did not update document'});
+      });
     });
