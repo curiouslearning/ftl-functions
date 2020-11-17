8375e71870bb82e97c42b78f5a14a5e7eaa25c25
Eric GT <bookjunkie93@gmail.com>
2020-11-17 17:19:21 -0500

    onNewUser: all tests passing

--- functions/onNewUser.js
+++ functions/onNewUser.js
@@ -8,19 +8,37 @@ const firestore = admin.firestore();
 
 exports.onNewUser = functions.firestore
     .document('user_pool/{docId}').onCreate((snap, context)=>{
-      const newDoc = snap.data();
-      if (!newDoc.countedInMasterCount) {
-        helpers.updateMasterLearnerCount(newDoc.country);
-      } if (!newDoc.countedInRegion) {
-        helpers.updateCountForRegion(newDoc.country, newDoc.region);
-      } if (!newDoc.countedInCampaign) {
-        helpers.updateCountForCampaign(newDoc.sourceCampaign);
+      try {
+        const newDoc = snap.data();
+        if (newDoc.countedInMasterCount && newDoc.countedInRegion &&
+        newDoc.countedInCampaign) {
+          return {
+            status: 200,
+            data: `${snap.id} is already counted`,
+          };
+        }
+        if (!newDoc.countedInMasterCount) {
+          helpers.updateMasterLearnerCount(newDoc.country);
+        } if (!newDoc.countedInRegion) {
+          helpers.updateCountForRegion(newDoc.country, newDoc.region);
+        } if (!newDoc.countedInCampaign) {
+          helpers.updateCountForCampaign(newDoc.sourceCampaign);
+        }
+        return snap.ref.update({
+          countedInMasterCount: true,
+          countedInRegion: true,
+          countedInCampaign: true,
+        }).then((res) => {
+          return {
+            status: 200,
+            data: `${context.params.docId} successfully counted`
+          };
+        }).catch((err)=>{
+          console.log(err);
+          return {status: 501, data: `encountered an error! ${err}`};
+        });
+      } catch (e) {
+        console.log(e);
+        return {status: 501, data: `encountered an error! ${e}`};
       }
-      return snap.ref.update({
-        countedInMasterCount: true,
-        countedInRegion: true,
-        countedInCampaign: true,
-      }).catch((err)=>{
-        console.error(err);
-      });
     });
--- test/functions/onNewUser.spec.js
+++ test/functions/onNewUser.spec.js
@@ -2,9 +2,16 @@ const test = require('firebase-functions-test')();
 const sinon = require('sinon');
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-
-beforeEach(() => {});
-afterEach(() => {});
+const helpers = require('../../functions/helpers/firebaseHelpers');
+const sandbox = require('sinon').createSandbox();
+beforeEach(() => {
+  sandbox.restore();
+  adminInitStub.restore();
+  adminInitStub = sinon.stub(admin, 'initializeApp');
+});
+afterEach(() => {
+  adminInitStub.restore();
+});
 describe('functions/onNewUser', async () => {
   const myFunction = require('../../functions/onNewUser');
   const firestore = admin.firestore.Firestore;
@@ -13,10 +20,78 @@ describe('functions/onNewUser', async () => {
   let updateMasterCountStub;
   let updateRegionsStub;
   let updateCampaignStub;
-  let getStub;
-  beforeEach(() => {});
-  afterEach(() => {});
-  it('should throw a missing argument error', async () => {});
-  it('should return the new counts', async () => {});
-  it('should not run', async () => {});
+  let updateStub;
+  let wrapped;
+  beforeEach(() => {
+    snap = {
+      id: 'fake-user',
+      data: () => {
+        return {
+          userID: 'fake-user',
+          country: 'fake-country',
+          region: 'fake-region',
+          dateCreated: firestore.Timestamp.now(),
+          userStatus: 'unassigned',
+          sourceCampaign: 'fakeCampaign',
+          sourceDonor: 'unassigned',
+          learnerLevel: 'first-open',
+          countedInMasterCount: false,
+          countedInCampaign: false,
+          countedInRegion: false,
+        };
+      },
+      ref: {
+        update: () => {
+          return new Promise((res)=>{
+            return res(sinon.fake());
+          });
+        },
+      },
+    };
+    context = {
+      params: {
+        docId: 'fake-user',
+      },
+    };
+    updateMasterCountStub = sandbox.stub(helpers, 'updateMasterLearnerCount');
+    updateRegionsStub = sandbox.stub(helpers, 'updateCountForRegion');
+    updateCampaignStub = sandbox.stub(helpers, 'updateCountForCampaign');
+    updateStub = sandbox.stub(firestore.DocumentReference.prototype, 'update');
+    wrapped = test.wrap(myFunction.onNewUser);
+  });
+  afterEach(() => {
+    sandbox.restore();
+  });
+  it('should throw a missing argument error', async () => {
+    updateCampaignStub.throws('Argument-Error', 'missing argument');
+    const res = await wrapped(snap, context);
+    res.should.deep.equal({
+      status: 501,
+      data: 'encountered an error! Argument-Error: missing argument',
+    });
+  });
+  it('should return a message on success', async () => {
+    const res = await wrapped(snap, context);
+    res.should.deep.equal({
+      status: 200,
+      data: 'fake-user successfully counted',
+    });
+  });
+  it('should not run if user is already counted', async () => {
+    snap = {
+      id: 'fake-user',
+      data: () => {
+        return {
+          countedInCampaign: true,
+          countedInMasterCount: true,
+          countedInRegion: true,
+        };
+      },
+    };
+    const res = await wrapped(snap, context);
+    res.should.deep.equal({
+      status: 200,
+      data: 'fake-user is already counted',
+    });
+  });
 });
