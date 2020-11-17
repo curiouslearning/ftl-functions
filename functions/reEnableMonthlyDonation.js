e2e95ab6ccb008ac3b8dc8aaa2cc9a4b8a12087b
Eric GT <bookjunkie93@gmail.com>
2020-11-17 18:05:29 -0500

    reEnableMonthlyDonation: all checks passing

--- functions/reEnableMonthlyDonation.js
+++ functions/reEnableMonthlyDonation.js
@@ -6,7 +6,7 @@ if (!admin.app) {
 }
 exports.reEnableMonthlyDonation = functions.firestore
     .document('donor_master/{uid}/donations/{donationId}')
-    .onUpdate((change, context)=>{
+    .onUpdate((change, context) => {
       const before = change.before.data();
       const after = change.after.data();
       // learnerCount never decreases so a drop in percentFilled
@@ -14,6 +14,14 @@ exports.reEnableMonthlyDonation = functions.firestore
       if (before.percentFilled > after.percentFilled) {
         // removing the end date is the final step to allowing a monthly
         // donation to receive users again.
-        after.ref.update({endDate: admin.firestore.FieldValue.delete()});
+        return change.after.ref.update({
+          endDate: admin.firestore.FieldValue.delete(),
+        }).then((res) => {
+          return {status: 200, data: `updated ${context.params.donationId}`};
+        }).catch((err) => {
+          console.log(err);
+          return {status: 501, data: `encountered an error! ${err}`};
+        });
       }
+      return {status: 200, data: `no change necessary for ${context.params.donationId}`};
     });
--- test/functions/reEnableMonthlyDonation.spec.js
+++ test/functions/reEnableMonthlyDonation.spec.js
@@ -2,4 +2,92 @@ const test = require('firebase-functions-test')();
 const sinon = require('sinon');
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-const myFunction = require('../../functions/batchManager');
+const sandbox = require('sinon').createSandbox();
+
+beforeEach(() => {
+  adminInitStub.restore();
+  adminInitStub = sinon.stub(admin, 'initializeApp');
+});
+
+afterEach(() => {
+  adminInitStub.restore();
+  sandbox.restore();
+});
+
+describe('functions/reEnableMonthlyDonation', async () => {
+  const myFunction = require('../../functions/reEnableMonthlyDonation');
+  const firestore = admin.firestore.Firestore;
+  let before;
+  let after;
+  let context;
+  let updateStub;
+  let deleteStub;
+  let wrapped;
+  beforeEach(() => {
+    before = {
+      data: () => {
+        return {
+          percentFilled: 30,
+        };
+      },
+    };
+    after = {
+      data: () => {
+        return {
+          percentFilled: 60,
+        };
+      },
+      ref: {
+        update: sinon.fake(()=>{return new Promise((res)=>{return res(1);});}),
+      },
+    };
+    context = {};
+    updateStub = sandbox.stub(firestore.DocumentReference.prototype, 'update');
+    updateStub.returns(new Promise((res)=>{
+      return res(1);
+    }));
+    deleteStub = sandbox.stub(admin.firestore.FieldValue, 'delete');
+    deleteStub.returns('deleted');
+    wrapped = test.wrap(myFunction.reEnableMonthlyDonation);
+  });
+  afterEach(() => {
+    sandbox.restore();
+  });
+  it('should not run if percentFilled is increased', async () => {
+    const change = test.makeChange(before, after);
+    const res = await wrapped(change, context);
+    after.ref.update.should.not.have.been.called;
+  });
+  it('should run if percentFilled has decreased', async () => {
+    before = {
+      data: () => {
+        return {
+          percentFilled: 100,
+        };
+      },
+    };
+    const change = test.makeChange(before, after);
+    await wrapped(change, context);
+    after.ref.update.should.have.been.calledWith({endDate: 'deleted'});
+  });
+  it('should return an error', async () => {
+    before = {
+      data: () => {
+        return {
+          percentFilled: 100,
+        };
+      },
+    };
+    after.ref.update = sandbox.fake(()=>{
+      return new Promise((res, rej)=>{
+        rej(new Error('you failed!'));
+      });
+    });
+    const change = test.makeChange(before, after);
+    const res = await wrapped(change, context);
+    res.should.deep.equal({
+      status: 501,
+      data: 'encountered an error! Error: you failed!',
+    });
+  });
+});
